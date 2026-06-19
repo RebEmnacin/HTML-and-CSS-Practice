566fe887acac62a1f2e8734497263df696c73a72
RebEmnacin <rebqemnacin@gmail.com>
2026-06-19 12:40:29 +0800

    update

---
+++ LearnJavaScript/PassengerCounter/index.js
@@ -0,0 +1,38 @@
+let lapsCompleted = 0
+function incrementLap() {
+    lapsCompleted = lapsCompleted + 1
+}
+
+    incrementLap()
+    incrementLap()
+    incrementLap()
+    console.log(lapsCompleted)
+
+
+
+
+
+
+
+
+//document.getElementById("count-el").innerText = 5
+
+// let firstBatch = 5
+// let secondBatch = 7
+//let count = firstBatch + secondBatch
+//console.log(count)
+
+//let myAge = 19
+//let humanDogRatio = 7
+//let myDogAge = myAge*humanDogRatio
+//console.log(myDogAge)
+
+//let bonusPoints = 50
+//console.log(bonusPoints)
+//bonusPoints = bonusPoints + 50
+//console.log(bonusPoints)
+//bonusPoints = bonusPoints - 75
+//console.log(bonusPoints)
+//bonusPoints = bonusPoints + 45
+//console.log(bonusPoints)
+
